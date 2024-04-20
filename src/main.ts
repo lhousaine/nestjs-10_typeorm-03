17ff4a7236248eeba93250b039c44d3b688a6a5f
lhouarhou <l.ouarhou.ensa@gmail.com>
2024-04-20 18:51:42 +0100

    feat: add cors and helmet

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+  app.use(helmet());
+  app.enableCors();
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
