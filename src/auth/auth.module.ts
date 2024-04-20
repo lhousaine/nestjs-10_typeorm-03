a6d32ab12edca0450fd85497796fd6ed3ebf2a2a
lhouarhou <l.ouarhou.ensa@gmail.com>
2024-04-20 19:36:24 +0100

    feat: add auth entities to typeorm module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,3 +1,4 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
@@ -5,10 +6,20 @@ import { UsersModule } from '../users/users.module';
 import { JwtModule } from '@nestjs/jwt';
 import { APP_GUARD } from '@nestjs/core';
 import { AuthGuard } from './auth.guard';
+import { MysqlDataSourceOptions } from '../typeorm.config';
+import { Client } from './entities/client.entity';
+import { Token } from './entities/token.entity';
+import { AuthScope } from './entities/scope.entity';
+import { AuthCode } from './entities/auth_code.entity';
+import { User } from '../users/user.entity';
 
 @Module({
   imports: [
     UsersModule,
+    TypeOrmModule.forFeature(
+      [Client, Token, AuthScope, AuthCode],
+      MysqlDataSourceOptions
+    ),
     JwtModule.register({
       global: true,
       secret: process.env.API_JWT_SECRET,
